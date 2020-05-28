ca2532d94f581a95d37e3a33cfcb4db00715319e
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-28 20:17:48 +0100

    Adds Card component and css

--- listings/src/users/components/SingleUser.js
+++ listings/src/users/components/SingleUser.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import Icon from '../../shared/components/UIElements/Icon';
 import { Link } from 'react-router-dom';
+import Card from '../../shared/components/UIElements/Card';
 import './SingleUser.css'
 
 const SingleUser = (props) => {
   return (
     <li className="single-user">
-        <div className="single-user_content">
-            <Link to={`/${props.id}/listings`}>
-            <div className="single-user_image">
-                <Icon />
-            </div>
-            <div className="single-user_info">
-                <h2>{props.name}</h2>
-                <h3>
-                  {props.listingsCount} {props.listingsCount === 1 ? 'Listing' : 'Listings'}
-                </h3>
-            </div>
+            <Card className="single-user_content">
+              <Link to={`/${props.id}/listings`}>
+                <div className="single-user_image">
+                  <Icon />
+                </div>
+               <div className="single-user_info">
+                  <h2>{props.name}</h2>
+                    <h3>
+                      {props.listingsCount} {props.listingsCount === 1 ? 'Listing' : 'Listings'}
+                    </h3>
+                </div>
             </Link>
-        </div>
+            </Card>
+        
     </li>
   )
 }
