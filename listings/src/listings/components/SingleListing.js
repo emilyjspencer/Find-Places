23e34b74e954c428c8526ef468fc38d707f9aaf4
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-30 21:20:55 +0100

    Adds modal that slides in and out upon click

--- listings/src/listings/components/SingleListing.js
+++ listings/src/listings/components/SingleListing.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
+import Modal from '../../shared/components/UIElements/Modal';
 
 import './SingleListing.css';
 
 const SingleListing = props => {
-  return <li className="single-listing">
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+
+
+  const closeMapHandler = () => setShowMap(false);
+
+  
+  return (
+      <Fragment>
+          <Modal show={showMap} 
+          onCancel={closeMapHandler} 
+          header={props.address} 
+          contentClass="single-listing_modal-content"
+          footerClass="single-listing_modal-actions"
+          footer={<Button onClick={closeMapHandler}>Close</Button>}>
+              <div className="map-container"></div>
+              <h2>Maps of listing</h2>
+          </Modal>
+  <li className="single-listing">
       <Card className="single-listing_content">
       <div className="single-listing_image">
           <img src={props.image} alt={props.title} />
@@ -15,13 +35,15 @@ const SingleListing = props => {
               <p>{props.description}</p>
           </div>
           <div className="single-listing_actions">
-            <Button inverse>View listing on map</Button>
+            <Button inverse onClick={openMapHandler}>View listing on map</Button>
             <Button to={`/listings${props.id}`}>Edit listing</Button>
             <Button danger>Delete listing</Button>
           </div>
       </div>
     </Card>
   </li>
+  </Fragment>
+  )
 }
 
 
